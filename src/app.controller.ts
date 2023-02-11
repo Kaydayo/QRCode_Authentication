90903cb24eadda6850dc4c45389844e7a856bf69
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2023-02-11 16:34:20 +0100

    set up views for ejs template engine

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import * as fs from 'fs';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request,Response } from 'express';
+import * as qrcode from 'qrcode'
 
-@Controller()
+@Controller('qr-code')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
+  @Get('/generateCode')
+  async getQrCode(@Res() res:Response) {
+    const qrData = `${process.env.baseUrl}/qr-code/movies`;
+    const qrImage = await qrcode.toBuffer(qrData);
+    res.setHeader('Content-Type', 'image/png');
+    res.send(qrImage);
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/movies')
+  async getMovies(@Res() res:Response) {
+    const movies = JSON.parse(fs.readFileSync('src/movies.json', 'utf-8'));
+    return res.render('index', { movies });
   }
 }
